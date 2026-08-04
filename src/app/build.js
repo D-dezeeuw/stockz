002bb2fbe59f720509cb856dd6418453a986ad97
Danny de Zeeuw <danny@nekomedia.nl>
2026-08-04 18:40:02 +0000

    chore: stamp build 8c9372b

--- src/app/build.js
+++ src/app/build.js
@@ -16,10 +16,10 @@
  */
 
 /** Short SHA of the commit this build was cut from. */
-export const BUILD_SHA = '33553d0'
+export const BUILD_SHA = '8c9372b'
 
 /** When it was stamped, ISO-8601 to the minute. */
-export const BUILD_AT = '2026-08-04T18:34'
+export const BUILD_AT = '2026-08-04T18:40'
 
 /**
  * The build stamp as the footer shows it.
